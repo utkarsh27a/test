109e2c7ed3dc0eb074af1fdbc1f4084ebf88a490
Utkarsh <utkarsh27a@gmail.com>
2016-09-27 13:45:39 +0530

    changes

--- hello.php
+++ hello.php
@@ -1,18 +1,30 @@
 <?php
-$str = 'abcdefdfvcsdfclkoubbsdsasafmkjnkwq';
+$str = 'abcdefdvcsdfclkoubbszfffffijklmnopqrstvwxyzgh';
 $arr = Array();
-
+$abc = Array();
+$count = 0;
 for($i=0;$i<strlen($str);$i++){
 
     $arr[$i] = substr($str,$i,1);
     
-	echo $arr[$i]."<br>";
+	//echo $arr[$i];
+    $val =  ord($arr[$i]);
+    for($k=97;$k<=122;$k++){
+    	if($val == $k){
+          array_push($abc,$val);
+    	}
+    	
+    }
 
 
 }
+//echo $count;
+$a = array_unique($abc);
+$count =  count($a);
+if($count==26){
+echo "all letter";
+}else{
 
-
-
-
-
+	echo "not all";
+}
 ?>
